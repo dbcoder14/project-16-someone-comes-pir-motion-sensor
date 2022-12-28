b9b75ad0b6e76d62dba6370e695579424a397191
dbCoder <96175955+dbcoder14@users.noreply.github.com>
2022-12-28 11:19:14 +0100

    updates

--- main.ts
+++ main.ts
@@ -4,11 +4,9 @@ basic.forever(function () {
     sensor = pins.digitalReadPin(DigitalPin.P0)
     if (sensor) {
         basic.showNumber(pins.digitalReadPin(DigitalPin.P0))
-        for (let index = 0; index < 10; index++) {
-            pins.analogWritePin(AnalogPin.P1, 1023)
-            basic.pause(5)
-            pins.analogWritePin(AnalogPin.P1, 41)
-        }
+        pins.analogWritePin(AnalogPin.P1, 1023)
+        basic.pause(5)
+        pins.analogWritePin(AnalogPin.P1, 41)
         basic.pause(500)
     } else {
         basic.showNumber(0)
